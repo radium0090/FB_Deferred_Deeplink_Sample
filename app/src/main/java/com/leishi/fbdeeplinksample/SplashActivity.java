e56ebc1e40c8722713e1b1b85ad6205d9cbc91e9
皮皮 <pp@pipideMacBook-Pro.local>
2018-10-24 22:46:56 +0900

    add new source

---
+++ app/src/main/java/com/leishi/fbdeeplinksample/SplashActivity.java
@@ -0,0 +1,18 @@
+package com.leishi.fbdeeplinksample;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.AppLaunchChecker;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+
+    }
+}
